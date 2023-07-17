fb437d816aabe09b0aac4d7142d98c54b5151b7a
shinku4269 <kouchan_1125@yahoo.co.jp>
2023-07-17 20:20:07 +0900

    新規登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters 
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_furikana, :first_name_furikana, :birth_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :family_name, :first_name, :family_name_furigana, :first_name_furigana, :birth_day])
     devise_parameter_sanitizer.permit(:sign_in)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,            presence: true
-  validates :email,               presence: true, uniqueness: true, inclusion: { in: ["@"]}
-  validates :encrypted_password,  presence: true, length: { minimum: 6 },
-                                  format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :password,  format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :family_name,         presence: true, 
                                   format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name,          presence: true, 
