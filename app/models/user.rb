e8473016ffee49cf10a3f65444d0ec5a08af0d18
shinku4269 <kouchan_1125@yahoo.co.jp>
2023-07-17 18:48:54 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,            presense: true
+  validates :email,               presense: true, uniqueness: true, inclusion: { in: ["@"]}
+  validates :encrypted_password,  presense: true, length: { minimum: 6 },
+                                  format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :family_name,         presense: true, 
+                                  format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name,          presense: true, 
+                                  format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_furigana,presense: true,
+                                  format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_furigana, presense: true,
+                                  format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :birth_day,           presense: true
+   
+  
 end
