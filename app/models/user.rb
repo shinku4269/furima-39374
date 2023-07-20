99f3471d20f73ea08129953f2f7614a958a136c5
shinku4269 <kouchan_1125@yahoo.co.jp>
2023-07-20 22:48:40 +0900

    細部の修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   validates :nickname,            presence: true
   validates :password,  format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :family_name,         presence: true, 
-                                  format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+                                  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :first_name,          presence: true, 
-                                  format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+                                  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :family_name_furigana,presence: true,
                                   format: { with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_furigana, presence: true,
