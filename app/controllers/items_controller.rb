52f8d7caaf475ffc20a7dc0df465279d20d3281b
shinku4269 <kouchan_1125@yahoo.co.jp>
2023-07-23 15:46:22 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:items).permit(:name, :description, :price, :category_id, :status_id, :shipping_charges_id, :shipping_area_id, :shipping_day_id, :image).merge(user_id: current_user.id)
+  end
 end
