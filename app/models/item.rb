58571a9117f76e2591db0cc81d8502db5e8625b8
shinku4269 <kouchan_1125@yahoo.co.jp>
2023-07-23 16:01:27 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,10 +9,13 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_charges
   belongs_to_active_hash :shipping_day
 
-  
-  validates :category_id         { other_than: 1, message: "Category can't be blank" }
-  validates :status_id           { other_than: 1, message: "Status can't be blank" }
-  validates :shipping_area_id    { other_than: 1, message: "Shipping_area can't be blank" }
-  validates :shipping_charges_id { other_than: 1, message: "Shipping_chages can't be blank" }
-  validates :shipping_day_id     { other_than: 1, message: "Shipping_day can't be blank" }
+  validates :name,                presence: true
+  validates :description,         presence: true
+  validates :price,               presence: true, format: { with: /\A[0-9]+\z/ },
+                                  numericality: { in: 300..9999999 }
+  validates :category_id,         presence: true, numericality: { other_than: 1, message: "Category can't be blank" }
+  validates :status_id,           presence: true, numericality: { other_than: 1, message: "Status can't be blank" }
+  validates :shipping_area_id,    presence: true, numericality: { other_than: 1, message: "Shipping_area can't be blank" }
+  validates :shipping_charges_id, presence: true, numericality: { other_than: 1, message: "Shipping_chages can't be blank" }
+  validates :shipping_day_id,     presence: true, numericality: { other_than: 1, message: "Shipping_day can't be blank" }
 end
