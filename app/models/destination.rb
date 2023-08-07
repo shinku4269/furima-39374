aad6bedbdc8ab3bfa912d528a29ee7aaab9e97ed
shinku4269 <kouchan_1125@yahoo.co.jp>
2023-08-07 19:59:18 +0900

    バリデーションとアソシエーション

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,15 @@
 class Destination < ApplicationRecord
+  belongs_to :purchase
+  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :shipping_area
+
+  validates :post_code,        presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+  validates :shipping_area_id, presence: true, numericality: { other_than: 1 }
+  validates :city,             presence: true
+  validates :address,          presence: true
+  validates :building_name,    presence: true
+  validates :phone_number,     presence: true,format: {with: /\A[0-9]+\z/}, length:  {minimum: 10, maximum: 11 }
+  
 end
