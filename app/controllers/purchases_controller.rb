da83d2d8ddb0da8d80c8ba6d03d5656b8ca19a80
shinku4269 <kouchan_1125@yahoo.co.jp>
2023-08-13 18:01:51 +0900

    購入の有無による表示の設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,7 @@
 class PurchasesController < ApplicationController
 
+  before_action :authenticate_user!
+  
   def index
     @item = Item.find(params[:item_id])
     @purchase_destination = PurchaseDestination.new
