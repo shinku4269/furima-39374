67c28929f73f03a882e7e86a22ddd4bedfdca41c
shinku4269 <kouchan_1125@yahoo.co.jp>
2023-08-13 17:24:50 +0900

    クレジット決済機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,6 +9,7 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_destination = PurchaseDestination.new(purchase_params)
     if @purchase_destination.valid?
+      pay_item
       @purchase_destination.save
       redirect_to root_path
     else
@@ -19,6 +20,15 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_destination).permit(:post_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :purchase_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_destination).permit(:post_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :purchase_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -1,6 +1,6 @@
 class PurchaseDestination
   include ActiveModel::Model
-  attr_accessor :post_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :purchase_id
+  attr_accessor :post_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :purchase_id, :token
 
   with_options presence: true do
     validates :post_code,        format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
@@ -10,6 +10,7 @@ class PurchaseDestination
     validates :phone_number,     format: {with: /\A[0-9]+\z/}, length: { minimum: 10, maximum: 11 }
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save
